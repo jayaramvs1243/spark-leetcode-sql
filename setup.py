f06f5181435226107911fb6f5fc0f6a89c74cb8d
Jayaram Jai <jayaramvs1243@gmail.com>
2024-03-07 00:14:34 +0530

    Initial Project Configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Resolving LeetCode SQL problems using Spark',
+    author='Jayaram Jai',
+    license='',
+)
